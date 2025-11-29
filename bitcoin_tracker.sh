3024637c692fcebf39dc791b6ab99b5b0dd25bea
jwt1f24 <jwpro637@gmail.com>
2025-11-29 15:40:43 +0800

    (Commit message) Added a syntax to check if network or website is down or not

--- bitcoin_tracker.sh
+++ bitcoin_tracker.sh
@@ -3,9 +3,16 @@
 btc_src="https://coinmarketcap.com/currencies/bitcoin/"
 btc_file="btc_data.html"
 
+# check if network is down or not
+if ping -c 1 8.8.8.8; then
+	echo "Network is available! Proceeding..."
+else
+	echo "Error! Network is down..."
+	exit 1
+fi
+
 # fetch website data and save output in a new file
 curl "$btc_src" > "$btc_file"
-
 
 # search for money values, clean and reformat data into numbers, and arrange data in an array
 btc_data=($(grep -o "\$[0-9,]\+\.[0-9]\+" "$btc_file" | sed "s/[$,]//g" | awk '{printf "%.2f\n", $1}'))
