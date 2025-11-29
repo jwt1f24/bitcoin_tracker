3e64512e909e22e0c6bcbb91dc4e22837c92063a
jwt1f24 <jwpro637@gmail.com>
2025-11-29 16:01:41 +0800

    (Commit message) Made some minor changes to syntax for neater output in bash

--- bitcoin_tracker.sh
+++ bitcoin_tracker.sh
@@ -14,12 +14,12 @@ fi
 # fetch website data and save output in a new file
 curl "$btc_src" > "$btc_file"
 
-# check if website is blocking the script or not
-if grep 'data-test="text-cdp-price-display"' "$btc_file"; then
+# check if script is blocked by website or not
+if grep -q 'data-test="text-cdp-price-display"' "$btc_file"; then
 	echo "Script is not blocked, proceeding..."
 else
 	echo "Error! Script is blocked."
-	exit 1
+	exit 2
 fi
 
 # search for money values, clean and reformat data into numbers, and arrange data in an array
@@ -27,6 +27,3 @@ btc_data=($(grep -o "\$[0-9,]\+\.[0-9]\+" "$btc_file" | sed "s/[$,]//g" | awk '{
 btc_usd=${btc_data[0]}
 btc_low_24h=${btc_data[13]}
 btc_high_24h=${btc_data[14]}
-echo "$btc_usd"
-echo "$btc_low_24h"
-echo "$btc_high_24h"
