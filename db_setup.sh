0c15e7cf5976e045cd0a57fdb617db33bfdb8edc
jwt1f24 <jwpro637@gmail.com>
2025-11-30 16:37:12 +0800

    (Commit message) Added table to store names of various cryptocurrencies

--- db_setup.sh
+++ db_setup.sh
@@ -8,5 +8,10 @@ mysql -u "$user" <<EOFMYSQL
 CREATE DATABASE bitcoin_tracker;
 USE bitcoin_tracker;
 
+CREATE TABLE cryptocurrency (
+	currencyID INT AUTO_INCREMENT PRIMARY KEY UNIQUE NOT NULL,
+	name VARCHAR(50) NOT NULL
+);
+
 EOFMYSQL
 echo "Database is successfully created!"
