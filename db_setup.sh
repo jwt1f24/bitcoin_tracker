4d1b8ace72fae4d4ac9c87e13806843587652128
jwt1f24 <jwpro637@gmail.com>
2025-11-30 16:32:37 +0800

    (Commit message) Added delimiters for database creation setup

--- db_setup.sh
+++ db_setup.sh
@@ -3,4 +3,10 @@
 user="root"
 
 # activate mysql to create database and tables
-mysql -u "$user"
+mysql -u "$user" <<EOFMYSQL
+
+CREATE DATABASE bitcoin_tracker;
+USE bitcoin_tracker;
+
+EOFMYSQL
+echo "Database is successfully created!"
