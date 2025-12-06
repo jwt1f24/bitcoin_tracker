c3c039f334ec1ab64a9cdce981d3ec452ff56ddb
jwt1f24 <jwpro637@gmail.com>
2025-12-06 23:57:54 +0800

    (Commit message) Added GNU plot syntax inside function

--- plot.sh
+++ plot.sh
@@ -4,5 +4,10 @@ dbname="bitcoin_tracker"
 user="root"
 
 bitcoinpriceusd() {
-
+	gnuplot <<EOF
+	set terminal png font 'Arial'
+	set title 'Bitcoin USD Prices'
+	set output 'image.png'
+	plot "outfile.dat" u 1:2 w l t 'data'
+	EOF
 }
