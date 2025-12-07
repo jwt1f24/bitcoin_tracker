398610a6ef8f87eb94d712bae4511af1924f3fa7
jwt1f24 <jwpro637@gmail.com>
2025-12-07 21:31:45 +0800

    (Commit message) Added MYSQL syntax to write selected data into a file for creating the plot

--- plot.sh
+++ plot.sh
@@ -3,11 +3,19 @@
 dbname="bitcoin_tracker"
 user="root"
 
+# plot 1: get bitcoin USD prices
 bitcoinpriceusd() {
-	gnuplot <<EOF
+mysql -u "$user" -e "
+USE bitcoin_tracker;
+SELECT datecollected, price_usd FROM prices WHERE currencyID = 1 ORDER BY datecollected;
+" > outfile.dat
+
+gnuplot <<EOF
 	set terminal png font 'Arial'
-	set title 'Bitcoin USD Prices'
 	set output 'image.png'
+	set title 'Bitcoin USD Prices'
+	set xlabel 'Date Collected'
+	set ylabel 'Price (USD)'
 	plot "outfile.dat" u 1:2 w l t 'data'
-	EOF
+EOF
 }
