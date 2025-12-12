92c890e93e56227def5bdbfbfc25e1d3549b4b92
jwt1f24 <jwpro637@gmail.com>
2025-12-12 23:54:36 +0800

    (Commit message) Added function to plot Ethereum USD prices in a day

--- plot.sh
+++ plot.sh
@@ -30,3 +30,29 @@ gnuplot <<EOF
 	plot "outfile.dat" u 1:2 w lp lw 2 lc rgb 'red' pt 2 t 'Bitcoin'
 EOF
 }
+
+# plot 2: get Ethereum USD prices
+ethereumprice() {
+mysql -u "$user" -N -e "
+USE bitcoin_tracker;
+SELECT datecollected, price_usd FROM prices WHERE currencyID = 2
+AND datecollected BETWEEN '2025-12-10 00:00:00' AND '2025-12-11 00:00:59';
+" > outfile.dat
+
+gnuplot <<EOF
+        set terminal png font 'Arial' size 1280, 720
+        set output 'ethereumprice.png'
+        set title 'Ethereum USD Prices in a Day'
+        set xlabel 'Date Collected'
+        set ylabel 'Price USD ($)'
+        set xdata time
+        set timefmt "%Y-%m-%d %H:%M:%S" # format time in output file
+        set format "%d-%m\n%H:%M" # output time in image
+        set datafile separator "\t" # separate data into columns
+        set grid
+        set yrange [2000:4000]
+        set ytics 2000, 200, 4000
+        set format y "%.2f"
+        plot "outfile.dat" u 1:2 w lp lw 2 lc rgb 'red' pt 2 t 'Ethereum'
+EOF
+}
