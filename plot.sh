0371ae5b5f56cab9b304a2a1a292adf3368beaa9
jwt1f24 <jwpro637@gmail.com>
2025-12-12 23:43:16 +0800

    (Commit message) Adjusted plotting format and image size

--- plot.sh
+++ plot.sh
@@ -14,11 +14,11 @@ AND datecollected BETWEEN '2025-12-10 00:00:00' AND '2025-12-11 00:00:59';
 
 # create plot
 gnuplot <<EOF
-	set terminal png font 'Arial'
-	set output 'image.png'
-	set title 'Bitcoin USD Prices'
+	set terminal png font 'Arial' size 1280, 720
+	set output 'bitcoinprice.png'
+	set title 'Bitcoin USD Prices in a Day'
 	set xlabel 'Date Collected'
-	set ylabel 'Price (USD)'
+	set ylabel 'Price USD ($)'
 	set xdata time
 	set timefmt "%Y-%m-%d %H:%M:%S" # format time in output file
 	set format "%d-%m\n%H:%M" # output time in image
@@ -27,6 +27,6 @@ gnuplot <<EOF
 	set yrange [80000:100000]
 	set ytics 80000, 2000, 100000
 	set format y "%.2f"
-	plot "outfile.dat" u 1:2 w l lc rgb 'red' t 'data'
+	plot "outfile.dat" u 1:2 w lp lw 2 lc rgb 'red' pt 2 t 'Bitcoin'
 EOF
 }
