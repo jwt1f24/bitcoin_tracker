8742aeb5cd357ab3f19453dc43eb434abdd67663
jwt1f24 <jwpro637@gmail.com>
2025-12-13 12:26:34 +0800

    (Commit message) Added function to plot lowest & highest Bitcoin prices accross one week

--- plot.sh
+++ plot.sh
@@ -154,6 +154,41 @@ gnuplot <<EOF
 EOF
 }
 
+# plot 6: compare 24hr highest & lowest price of Bitcoin
+bitcoin24hr() {
+mysql -u "$user" -N -e "
+USE bitcoin_tracker;
+SELECT datecollected, lowest_24h FROM prices WHERE currencyID = 1
+AND datecollected BETWEEN '2025-12-01' AND '2025-12-09'
+AND TIME(datecollected) BETWEEN '00:00:00' AND '00:00:59';
+" > outfile.dat
+mysql -u "$user" -N -e "
+USE bitcoin_tracker;
+SELECT datecollected, highest_24h FROM prices WHERE currencyID = 1
+AND datecollected BETWEEN '2025-12-01' AND '2025-12-09'
+AND TIME(datecollected) BETWEEN '00:00:00' AND '00:00:59';
+" > outfile2.dat
+
+gnuplot <<EOF
+        set terminal png font 'Arial' size 1280, 720
+        set output 'bitcoin24hr.png'
+        set title 'Highest & Lowest Bitcoin Prices in 24 Hours Across One Week'
+        set xlabel 'Date Collected'
+        set ylabel 'Price USD ($)'
+        set xdata time
+        set timefmt "%Y-%m-%d %H:%M:%S" # format time in output file
+        set format "%d-%m\n%H:%M" # output time in image
+        set datafile separator "\t" # separate data into columns
+        set grid
+        set yrange [80000:100000]
+        set ytics 80000, 2000, 100000
+        set format y "%.2f"
+        plot \
+                "outfile.dat" u 1:2 w lp lw 2 lc rgb 'red' pt 2 t 'Lowest Price', \
+                "outfile2.dat" u 1:2 w lp lw 2 lc rgb 'green' pt 2 t 'Highest Price'
+EOF
+}
+
 # parameters for executing function
 if [[ "$1" == "bitcoinprice" ]]; then
 	bitcoinprice
@@ -165,4 +200,6 @@ elif [[ "$1" == "bnbprice" ]]; then
         bnbprice
 elif [[ "$1" == "allprices" ]]; then
         allprices
+elif [[ "$1" == "bitcoin24hr" ]]; then
+        bitcoin24hr
 fi
