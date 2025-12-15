6a0af0991f716f73eb63393874b1f0e58a5624dc
jwt1f24 <jwpro637@gmail.com>
2025-12-15 14:57:35 +0800

    (Commit message) Rewrote script to import database dump file

--- db_setup.sh
+++ db_setup.sh
@@ -1,31 +1,12 @@
 #!/bin/bash
 
 user="root"
+dbdump="database/bitcoin_tracker.sql"
 
-# activate mysql to create database and tables
-mysql -u "$user" <<EOFMYSQL
-
-DROP DATABASE IF EXISTS bitcoin_tracker;
-CREATE DATABASE IF NOT EXISTS bitcoin_tracker;
-USE bitcoin_tracker;
-
-CREATE TABLE IF NOT EXISTS cryptocurrency (
-	currencyID INT AUTO_INCREMENT PRIMARY KEY UNIQUE NOT NULL,
-	name VARCHAR(50) NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS prices (
-        priceID INT AUTO_INCREMENT PRIMARY KEY UNIQUE NOT NULL,
-        currencyID INT NOT NULL,
-	price_usd DECIMAL(20, 2) NOT NULL,
-	lowest_24h DECIMAL(20, 2) NOT NULL,
-	highest_24h DECIMAL(20, 2) NOT NULL,
-	datecollected DATETIME NOT NULL,
-	FOREIGN KEY (currencyID) REFERENCES cryptocurrency(currencyID)
-);
-
-INSERT INTO cryptocurrency(name) VALUES
-("Bitcoin"), ("Ethereum"), ("XRP"), ("BNB");
-
-EOFMYSQL
-echo "Database and tables are successfully created! Data can be inserted now."
+# import database dump file
+if mysql -u "$user" < "$dbdump"; then
+	echo "Database dump file has been imported!"
+else
+	echo "Error! Import failed."
+	exit 1
+fi
